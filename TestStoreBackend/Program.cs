ab2a8bd7b5fde88f7600da0dc30f5b35b42e9ebd
Erik Holm <54987631+erikholm97@users.noreply.github.com>
2020-11-11 07:36:28 +0100

    Update Program.cs
    Login query

--- TestStoreBackend/Program.cs
+++ TestStoreBackend/Program.cs
@@ -11,19 +11,35 @@ namespace TestStoreBackend
         {
             using (var db = new StoreContext())
             {
-               
+                User user = new User()
+                {
+                    FirstName = "Erik",
+                    LastName = "Holm",
+                    Password = 123
+                };
+
+                db.User.Add(user);
+                db.SaveChanges();
+
+                var userIsValid = from u in db.User where u.FirstName == "Erik" && u.Password == 123 select u;
+
+                if(userIsValid.Count() >= 0)
+                {
+                    Console.WriteLine("You are logged in!");
+                }
+
                 // Read
-                var usr = db.User
-                    .OrderBy(b => b.FirstName)
-                    .First();
-                Console.ReadLine();
-                Console.WriteLine(usr.FirstName);
+                //var usr = db.User
+                //    .OrderBy(b => b.FirstName)
+                //    .First();
+                //Console.ReadLine();
+                //Console.WriteLine(usr.FirstName);
 
 
                 // Delete
-                Console.WriteLine("Delete user");
-                db.Remove(usr);
-                db.SaveChanges();
+                //Console.WriteLine("Delete user");
+                //db.Remove(usr);
+                //db.SaveChanges();
             }    
             // Todo Hämta ut information i databasen; // Check
 
